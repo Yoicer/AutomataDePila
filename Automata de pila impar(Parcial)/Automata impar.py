89e3146349eddd779d19a7f24ebac722cefe3e33
Yoicer <36748297+Yoicer@users.noreply.github.com>
2018-04-12 00:56:57 -0500

    Update Automata impar.py
    Versión de escritorio

--- Automata de pila impar(Parcial)/Automata impar.py
+++ Automata de pila impar(Parcial)/Automata impar.py
@@ -1,10 +1,9 @@
 import pila
+
 import tkinter
 from tkinter import Label
 from tkinter import *
 from tkinter import ttk
-
-
 
 class Autopi:
     def __init__(self, p):
@@ -40,67 +39,63 @@ class Autopi:
 #transiciones estando en estado 1 :)
     #transiciones con b
     def b_b_bb(self):
-        print('saca de la pila: ',self.pila.quitar())
+        self.pila.quitar()
         self.pila.apilar('b')
-        
         self.pila.apilar('b')
         self.activaEstado_1()
 
     def b_a_ab(self):
-        print('saca de la pila: ',self.pila.quitar())
+        self.pila.quitar()
         self.pila.apilar('a')
         self.pila.apilar('b')
         self.activaEstado_1()
     def b_n_nb(self):
-        print('saca de la pila: ',self.pila.quitar())
+        self.pila.quitar()
         self.pila.apilar('#')
-        print(self.pila.cima())
         self.pila.apilar('b')
         self.activaEstado_1()
 #transiciones con a
     def a_b_ba(self):
-        print('saca de la pila: ',self.pila.quitar())
+        self.pila.quitar()
         self.pila.apilar('b')
         self.pila.apilar('a')
         self.activaEstado_1()
     def a_n_na(self):
-        print('saca de la pila: ',self.pila.quitar())
+        self.pila.quitar()
         self.pila.apilar('#')
         self.pila.apilar('a')
         self.activaEstado_1()
     def a_a_aa(self):
-        print('saca de la pila: ',self.pila.quitar())
+        self.pila.quitar()
         self.pila.apilar('a')
         self.pila.apilar('a')
         self.activaEstado_1()
 #transiciones con c
     def c_n_n(self):
-        print('saca de la pila: ',self.pila.quitar())
+        self.pila.quitar()
         self.pila.apilar('#')
         print('apila:  #')
         self.activaEstado_2()
     def c_b_b(self):
-        print('saca de la pila: ',self.pila.quitar())
+        self.pila.quitar()
         self.pila.apilar('b')
-        print('apila: b')
         self.activaEstado_2()
     def c_a_a(self):
-        print('saca de la pila: ',self.pila.quitar())
+        self.pila.quitar()
         self.pila.apilar('a')
-        print('apila: a')
         self.activaEstado_2()
 
 #tansiciones estando en el estado 2 :)
     def b_b_y(self):
-        print('saca de la pila: ',self.pila.quitar())
+        self.pila.quitar()
         self.activaEstado_2()
 
     def a_a_y(self):
-        print('saca de la pila: ',self.pila.quitar())
+        self.pila.quitar()
         self.activaEstado_2()
 
     def y_n_n(self):
-        print('saca de la pila: ',self.pila.quitar())
+        self.pila.quitar()
         self.pila.apilar('#')
         self.activaEstado_final()
 
@@ -111,7 +106,7 @@ class Autopi:
         palabra=palabra+' '
         print('Estado 1  -  Estado 2  -  Estado Final  -  Cima Pila  -  Caracter')
         for caracter in palabra:
-            print(self.getEstado_1(),'           ', self.getEstado_2(),'         ',self.getEstado_final(),'               ', self.pila.cima(),'              ', caracter)
+            print(self.getEstado_1(),'           ', self.getEstado_2(),'         ',self.getEstado_final(),'    ', self.pila.cima(),'              ', caracter)
             if (caracter != 'a' and caracter != 'b' and caracter != 'c' and caracter != '#' and caracter != ' '):
                 print('El caracter es invalido en el lenguaje !!!')
                 break
@@ -168,55 +163,6 @@ pila=pila.Pila()
 auto= Autopi(pila)
 auto.prueba('abcba')
 
-def cargarlistbox(lista,listbox):
-    ind,largo=0,len(lista)
-    while ind < largo:
-        listbox.insert(END,lista[ind])
-        ind+=1
-def funcion():
-    otra_ventana = tkinter.Toplevel(root)
-    otra_ventana.minsize(width=600,height=500)
-
-    scrollbar = Scrollbar(otra_ventana)
-    scrollbar.pack(side=RIGHT, fill=Y)
-    
-    listbox = Listbox(otra_ventana)
-    listbox.insert(0, 'Hola :v')
-    cargarlistbox('holamundooooooooooooooooooooo',listbox)
-    
-    listbox.config(yscrollcommand=scrollbar.set)
-    scrollbar.config(command=listbox.yview)
-    listbox['bg'] = 'black'
-    listbox['fg'] = 'blue'
-
-    listbox.pack(side=LEFT, fill=BOTH, expand=1)
-    root.iconify()
-
-root = tkinter.Tk()
-
-img = PhotoImage(file="prueba.gif")
-widget = Label(root, image=img).pack()
-
-boton = tkinter.Button(root, text="Abrir otra ventana", command=funcion)
-boton['bg'] = 'black' 
-boton['fg'] = 'blue'
-boton.pack()
-
-listbox = Listbox(root)
-listbox.place(relwidth=1 ,relheight=-0.50)
-listbox['bg'] = 'black'
-listbox['fg'] = 'blue'
-listbox.pack(side=LEFT, fill=BOTH, expand=1)
-listbox.insert(END, 'Descripcion')
-
-scrollbar = Scrollbar(root)
-scrollbar.pack(side=RIGHT, fill=Y)
-
-listbox.config(yscrollcommand=scrollbar.set)
-scrollbar.config(command=listbox.yview)
-
-root.minsize(width=600,height=500)
-root.mainloop()
 
 
 
